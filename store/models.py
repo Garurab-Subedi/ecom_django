afd3db5cb98a696d65f885877e46caa358b0088e
Garurab-Subedi <gauaravsubedi299@gmail.com>
2025-07-05 11:02:21 +0545

    add sale and start

--- store/models.py
+++ store/models.py
@@ -25,6 +25,9 @@ class Product(models.Model):
     price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     image = models.ImageField(upload_to='uploads/products/', null=True, blank=True)
+    #Add sale Stuff
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
 
     def __str__(self):
         return self.name
